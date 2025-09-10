2416cabdabb9cfa9d22a9a5234fe0d41ce34d5ba
fathiaoyinloye <you@example.com>
2025-09-10 17:24:51 +0100

    Practice

---
+++ Value.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Values{
+	public static void main(String[] args){
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("Enter first number: ");
+	int first = scanner.nextInt();
+
+	System.out.print("Enter second number: ");
+	int second= scanner.nextInt();
+
+	System.out.print("Enter third number: ");
+	int third = scanner.nextInt();
+
+	System.out.print("Enter fourth number: ");
+	int fourth = scanner.nextInt();
+
+	System.out.print("Enter fifth number: ");
+	int fifth= scanner.nextInt();
+	
+	if(first > 0){
+	System.out.println(first + " is a positive number");
+	}
+	if(first < 0){
+	System.out.println(first + " is a negative number");
+	}
+	if(first == 0){
+	System.out.println(first + " is a equal to zero");
+	}
+	
+	if(second > 0){
+	System.out.println(second + " is a positive number");
+	}
+	if(second < 0){
+	System.out.println(second + " is a negative number");
+	}
+	if(second == 0){
+	System.out.println(second + " is a equal to zero");
+	}
+
+	if(third > 0){
+	System.out.println(third + " is a positive number");
+	}
+	if(third < 0){
+	System.out.println(third + " is a negative number");
+	}
+	if(third == 0){
+	System.out.println(third + " is a equal to zero");
+	}
+
+
+	if(fourth > 0){
+	System.out.println(fourth + " is a positive number");
+	}
+	if(fourth < 0){
+	System.out.println(fourth + " is a negative number");
+	}
+	if(fourth == 0){
+	System.out.println(fourth + " is a equal to zero");
+	}
+
+	if(fifth > 0){
+	System.out.println(fifth + " is a positive number");
+	}
+	if(fifth < 0){
+	System.out.println(fifth + " is a negative number");
+	}
+	if(fifth == 0){
+	System.out.println(fifth + " is a equal to zero");
+	}
+
+
+
+
+
+	
+	
+	}
+}
+
